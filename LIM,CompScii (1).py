2c2e2b56116e8d8c8fd330de67abeaf3ebf9c474
hermarara <hslim@cvisc.pshs.edu.ph>
2026-04-27 02:26:59 +0800

    Add files via upload

---
+++ LIM,CompScii (1).py
@@ -0,0 +1,20 @@
+students = []
+
+for i in range(3):
+    name = input("Enter student name: ")
+    age = int(input("Enter student age: "))
+    grade = input("Enter student grade: ")
+
+    student = {
+        "name": name,
+        "age": age,
+        "grade": grade
+    }
+
+    students.append(student)
+
+for student in students:
+    print("Name:", student["name"])
+    print("Age:", student["age"])
+    print("Grade:", student["grade"])
+    print("---")
---
+++ LimHermaLei_CS2_Q3_Act1.py
@@ -0,0 +1,11 @@
+Python 3.13.4 (tags/v3.13.4:8a526ec, Jun  3 2025, 17:46:04) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+= RESTART: C:/Users/LENOVO/AppData/Local/Programs/Python/Python313/LIM_CS_Async_Q3.py
+Enter your name: Herma 
+Enter your age: 13
+Enter your favorite subject: biology
+Student Record:
+Name: Herma 
+Age: 13
+Favorite Subject: biology
